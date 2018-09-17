f7db2704681322243f20be2a6dc751031c717d31
steven <nonkelchameau@gmail.com>
2018-09-17 09:26:54 +0200

    app.component hier kun je switchen om vb inline html te bekijken template url comment out zetten en template uncomment. ngIf is met boolean showProdukten, zet enkele produkten uncomment en zie resultaat.

--- WebBakker/angular-app/src/app/app.component.ts
+++ WebBakker/angular-app/src/app/app.component.ts
@@ -4,21 +4,29 @@ import { Produkt } from "./shared/model/produkt.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+//  template: `
+//<!-- Inline Html -->
+//<div class="container">
+//<h1>{{title}} + wat tekst</h1>
+//</div>
+//`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title: string;
   produkten: Produkt[];
+  showProdukten: boolean;
   constructor() {
     this.produkten = [
       new Produkt(1, 'Brood'),
-      new Produkt(1, 'Belegde broodjes'),
-      new Produkt(1, 'Kofiekoeken'),
-      new Produkt(1, 'Taart')
+      new Produkt(2, 'Belegde broodjes'),
+      new Produkt(3, 'Kofiekoeken'),
+      new Produkt(4, 'Taart')
     ]
   }
   ngOnInit() {
     this.title = 'Mijn produkten - via Model';
+    this.showProdukten = this.produkten.length > 3;
   }
 }
   
